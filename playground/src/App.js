fe5f6c3d422e553eb0cdba3910eef93d2b025a40
Gerardo Gallegos <puxerop@gmail.com>
2021-01-03 07:25:48 -0600

    adding the playground react app

---
+++ playground/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Cube3D } from 'test-npm-package'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Rocks</h1>
+      <Cube3D />
+    </div>
+  )
+}
+
+export default App
