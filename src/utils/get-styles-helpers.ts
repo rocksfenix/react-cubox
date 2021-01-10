ba0dca66387a0a9b20e2ce5997906ce8564ca908
Gerardo Gallegos <puxerop@gmail.com>
2021-01-09 21:04:45 -0600

    adding helpers for inline styles

---
+++ src/utils/get-styles-helpers.ts
@@ -0,0 +1,55 @@
+// Helpers to get inline styles
+
+// Styles for the main viewport container
+export const getViewportStyles = (size: number, other: {} | null) => ({
+  background: '#000',
+  marginTop: '20%',
+  perspective: '800px',
+  perspectiveOrigin: `50% ${size - size/6}px`,
+  transform: 'scale(0.8, 0.8)',
+  ...other,
+})
+
+interface CubeStyles {
+  size: number,
+  rotateX: number,
+  rotateY: number
+}
+
+// Styles for the cube container
+export const getCubeStyles = (cubeData: CubeStyles) => ({
+  position: 'relative',
+  margin: '0 auto',
+  transformStyle: 'preserve-3d',
+  willChange: 'transform',
+  height: `${cubeData.size}px`,
+  width: `${cubeData.size}px`,
+  transform: `rotateX(${cubeData.rotateX}deg) rotateY(${cubeData.rotateY}deg)`
+})
+
+// Styles for sides
+export const getSideStyles = (size: number, index: number) => ({
+  overflow: 'hidden',
+  position: 'absolute',
+  opacity: 0.8,
+  background: 'radial-gradient(blue, rgb(0, 0, 0))',
+  touchCallout: 'none',
+  userSelect: 'none',
+  height: size,
+  width: size,
+  willChange: 'opacity',
+  transition: 'opacity 500ms ease',
+  transform: getTransformBySide(index, size / 2)
+})
+
+function getTransformBySide (index: number, half: number) {
+  switch (index) {
+    case 1: return `rotateX(90deg) translateZ(${half}px)`
+    case 2: return `translateZ(${half}px)`
+    case 3: return `rotateY(90deg) translateZ(${half}px)`
+    case 4: return `rotateY(180deg) translateZ(${half}px)`
+    case 5: return `rotateY(-90deg) translateZ(${half}px)`
+    case 6: return `rotateX(-90deg) rotate(180deg) translateZ(${half}px)`
+  }
+}
+
