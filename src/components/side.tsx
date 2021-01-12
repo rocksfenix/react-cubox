6cb84db5df753e90070ceb6563e6433e87ae76d2
Gerardo Gallegos <puxerop@gmail.com>
2021-01-12 07:34:08 -0600

    creating side component for the cube side

--- src/components/side.tsx
+++ src/components/side.tsx
@@ -1,26 +1,67 @@
-import React, { useContext } from 'react'
-import context from '../context'
+import React from 'react'
+import { getCubeStyles, getSideStyles } from '../utils/get-styles-helpers'
+
+type event = {
+  side: number
+}
 
 interface SideProps {
-
+  index: number
+  onClick: (event: event) => void
+  size: number
+  rotateX: number
+  rotateY: number
 }
 
 export const Side: React.FC<SideProps> = (props) => {
-  const value: any = useContext(context)
+  function handleClick () {
+    if (props.onClick) {
+      props.onClick({ side: props.index, ...props })
+    }
+  }
+
+  // The children will be maped to add user-select none and
+  const globalChildren = React.Children.map(props.children, (child: any) => {
+    
+    return React.cloneElement(child, {
+      ...child.props,
+      style: {
+        ...child.props.style,
+        pointerEvents: 'none'
+      }
+    })
+  })
+
+  const cubeStyles = getCubeStyles({
+    size: props.size,
+    rotateX: props.rotateX,
+    rotateY: props.rotateY,
+  })
+
+  const sideStyles = getSideStyles(props.size, props.index)
+
+  const labelStyles = {
+    fontSize: 50,
+    color: 'white'
+  }
 
   return (
-    <div style={styles} {...props}>
-      {props.children} <span onClick={() => value.increment()}>{value.count}</span>
+    <div
+      style={cubeStyles}
+      {...props}
+      onClick={handleClick}
+    >
+      <div style={sideStyles}>
+        {!props.children && (
+          // Will be removed
+          <span style={labelStyles}>
+            {props.index}
+          </span>
+        )}
+        {props.children && globalChildren}
+      </div>
     </div>
   )
-}
-
-const styles = {
-  background: 'blue',
-  borderRadius: '0.3em',
-  padding: '1em',
-  margin: '1em',
-  color: '#FFF'
 }
 
 export default Side
