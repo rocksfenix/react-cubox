874c5ed90aa8790f04ea0548421a48cee7cf7252
Gerardo Gallegos <puxerop@gmail.com>
2021-02-16 08:09:21 -0600

    Default context values

--- src/context.ts
+++ src/context.ts
@@ -1,17 +1,20 @@
 import React from 'react'
+// import { Cube3DProps } from './types/interfaces'
 
 export interface CubeContext {
   size: number
   rotateX: number
   rotateY: number
   currentSide: number
+  bgColor: string
 }
 
 const context = React.createContext<CubeContext>({
   size: 200,
   rotateX: 0,
   rotateY: 0,
-  currentSide: 0
+  currentSide: 0,
+  bgColor: 'gold'
 })
 
 context.displayName = 'ReactCube3D'
