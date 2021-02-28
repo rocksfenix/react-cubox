546a4f84b6fd63cde015ac81e2b70a85f44bf968
Gerardo Gallegos <puxerop@gmail.com>
2021-02-28 08:14:00 -0600

    Documenting get-error-invalid-child

--- src/utils/get-error-invalid-child.ts
+++ src/utils/get-error-invalid-child.ts
@@ -1,13 +1,17 @@
 import React from 'react'
 import Face from '../components/face'
 
-// Helper function to check if we receive invalid
-// children component
+/**
+ * Description: Helper function to check if we receive invalid
+ * children component
+ * @param {React.ReactElement<any>} children
+ * @returns {string}
+ */
 export default function getErrorInvalidChild (children: React.ReactElement<any>) {
   let hasError = null
 
   if (!children) {
-    return `Error: You need to pass the <Face /> component inFace of <Cube /> component.`
+    return `Error: You need to pass the <Face /> component inside of <Cube /> component.`
   }
 
   React.Children.forEach(children, (child) => {
